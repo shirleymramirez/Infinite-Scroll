3bac3adaf203108c91440ff835ade88d2b6fcd2d
Shirley Ramirez <shirley.mramirez@yahoo.com>
2023-07-31 14:28:13 -0700

    Create infiniteScroll.js

---
+++ infiniteScroll.js
@@ -0,0 +1,63 @@
+const API_BASE_URL = 'https://api.frontendexpert.io/api/fe/testimonials';
+const PAGE_SIZE = 5;
+
+let canFetchTestimonials = true;
+let afterID = null;
+
+const testimonialContainer = document.getElementById('testimonial-container');
+
+testimonialContainer.addEventListener('scroll', handleScroll);
+
+fetchAndAppendTestimonials();
+
+function handleScroll() {
+  if (!canFetchTestimonials) return;
+  const bottomSpaceLeftToScroll = (
+    this.scrollHeight -
+    this.scrollTop -
+    this.clientHeight
+  );
+
+  if (bottomSpaceLeftToScroll > 0 ) return;
+  fetchAndAppendTestimonials()
+}
+
+function fetchAndAppendTestimonials() {
+  canFetchTestimonials = false;
+  const url = createTestimonialsUrl();
+  fetch(url)
+    .then(res => res.json());
+    .then(({testimonials, hasNext}) => {
+      const fragment = document.createDocumentFragment();
+      testimonials.forEach(({message}) => {
+        fragment.appendChild(createTestimonialElement(message));
+      });
+      testimonialContainer.appendChild(fragment);
+
+      if (hasNext) {
+        afterID = testimonials[testimonials.length - 1].id;
+      } else {
+        testimonialsContainer.removeEventListener('scroll', handleScroll);
+      }
+      canFetchTestimonials = true;
+    });
+}
+
+function createTestimonialElement(message) {
+  conts testimonialElement = document.createElement('p');
+  testimonialElement.classList.add('testimonial');
+  testimonialElement.textContent = message;
+  return testimonialElement;
+}
+
+function createTestimonialsUrl() {
+  const url = new URL(API_BASE_URL);
+  url.searchParams.set('limit', PAGE_SIZE);
+
+  if (afterID !== null) {
+    url.searchParams.set('after', afterID);
+  }
+  return url;
+  
+  //API_BASE_URL + '?limit=' + '5'
+}
